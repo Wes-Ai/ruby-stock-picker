43e69bdc67a136d0fd73e2b0187d68b87186e67b
Wesley <smithw26@wwu.edu>
2022-06-11 16:55:17 -0700

    Stock_picker logic working; testing edge cases.

--- stock.rb
+++ stock.rb
@@ -11,14 +11,18 @@ def stock_picker(stock_prices)
         profit = 0
         stock_prices.each_with_index do |stock, index|
             #Stores highest number with an array of indexes used
-            highest_profit[stock_prices[index] - stock_prices[0]] = 
+            x = stock_prices[index]
+            y = stock_prices[0]
+            highest_profit[x - y] = [stock_prices.index(x), stock_prices.index(y)]
         end
-        p highest_profit
         stock_prices.shift
     end
+    best_profit = highest_profit.sort.max
+
+    best_profit[1].reverse
 end 
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
-# p stock_picker([3,8,6,5,4,7,22,8,6])
+p stock_picker([3,8,6,5,4,7,22,8,6])
 # p stock_picker([25,3,8,6,5,4,7,22,8,6])
 # p stock_picker([25,3,8,6,5,4,7,22,8,6,1])
