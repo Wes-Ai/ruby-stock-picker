a39874411ad9b497b94649279d57557396def518
Wesley <smithw26@wwu.edu>
2022-06-11 16:22:49 -0700

    Psuedocode refactoring entire program.

--- stock.rb
+++ stock.rb
@@ -1,5 +1,15 @@
 def stock_picker(stock_prices)
-    stock_prices.minmax
+    #loop, 
+        #[0] - [every other variable]
+        #store in array (or hash?)
+        #pop off [0], [1] becomes next [0]
+        #repeat
+    #find highest profit in array (or hash)
+    #return indexes of highest profit.
+    
 end 
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([3,8,6,5,4,7,22,8,6])
+p stock_picker([25,3,8,6,5,4,7,22,8,6])
+p stock_picker([25,3,8,6,5,4,7,22,8,6,1])
