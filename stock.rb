32fa32ac8c23fad5e2c265e7e52add26330e0379
Wesley <smithw26@wwu.edu>
2022-06-12 13:13:44 -0700

    Complete rewrite, much simpler logic. Working with test cases.

--- stock.rb
+++ stock.rb
@@ -1,37 +1,23 @@
 def stock_picker(stock_prices)
-    #loop, 
-        #[0] - [every other variable]
-        #store in array (or hash?)
-        #pop off [0], [1] becomes next [0]
-        #repeat
-    #find highest profit in array (or hash)
-    #return indexes of highest profit.
-    highest_profit = Hash.new(0)
-    count = 0
-    while stock_prices.length > 1
+    highest_profit = 0;
+    days_pair = []
 
-        profit_sorter = Hash.new(0)
-        stock_prices.each_with_index do |stock, index|
-            #Stores highest number with an array of indexes used
-            x = stock_prices[index]
-            y = stock_prices[0]
-            profit_sorter[x - y] = [stock_prices.index(y), stock_prices.index(x)]
-            p profit_sorter
+    stock_prices.each_with_index do |price1, index1|
+        stock_prices.each_with_index do |price2, index2|
+            profit_check = price2 - price1
+
+            #Check for a higher profit and if the indexes are correctly arranged
+            if(profit_check > highest_profit && index1 < index2)
+                highest_profit = profit_check
+                days_pair = [index1, index2]
+            end
         end
-        #TODO: Add this into the loop, so that I get an array of max's, only.
-        p highest_profit[profit_sorter.sort.max[0]] = profit_sorter.sort.max[1]
-        
-        count = count + 1
-        stock_prices.shift
     end
-    p highest_profit
-    best_profit = highest_profit.sort.max
-
-    best_profit[1]
+    days_pair
 end 
 
-#p stock_picker([17,3,6,9,15,8,6,1,10])
-#p stock_picker([3,8,6,5,4,7,22,8,6])
-stock_picker([25,3,8,6,5,4,7,22,8,6])
-#p stock_picker([25,3,8,6,5,4,7,22,8,6,1])
-#p stock_picker([17,3,6,9,15,8,6,1,10])
+p "Expected: [1, 4], Actual: " + stock_picker([17,3,6,9,15,8,6,1,10]).to_s
+p "Expected: [0, 6], Actual: " + stock_picker([3,8,6,5,4,7,22,8,6]).to_s
+p "Expected: [1, 7], Actual: " + stock_picker([25,3,8,6,5,4,7,22,8,6]).to_s
+p "Expected: [1, 6], Actual: " + stock_picker([25,3,8,6,5,4,22,8,6,1,7]).to_s
+p "Expected: [7, 8], Actual: " + stock_picker([17,3,6,9,11,8,6,1,10]).to_s
