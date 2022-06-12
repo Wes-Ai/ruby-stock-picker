9ce3a223fad2759b6281b2e75d79a64e426d0a07
Wesley <smithw26@wwu.edu>
2022-06-11 22:03:11 -0700

    Working on logic pertaining to hashes and storing the max profits.

--- stock.rb
+++ stock.rb
@@ -8,21 +8,26 @@ def stock_picker(stock_prices)
     #return indexes of highest profit.
     highest_profit = Hash.new(0)
     while stock_prices.length > 1
-        profit = 0
+
+        profit_sorter = Hash.new(0)
         stock_prices.each_with_index do |stock, index|
             #Stores highest number with an array of indexes used
             x = stock_prices[index]
             y = stock_prices[0]
-            highest_profit[x - y] = [stock_prices.index(x), stock_prices.index(y)]
+            profit_sorter[x - y] = [stock_prices.index(y), stock_prices.index(x)]
         end
+        #TODO: Add this into the loop, so that I get an array of max's, only.
+        p highest_profit[:a] = profit_sorter.sort.max
+
         stock_prices.shift
     end
     best_profit = highest_profit.sort.max
 
-    best_profit[1].reverse
+    best_profit[1]
 end 
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
-p stock_picker([3,8,6,5,4,7,22,8,6])
-# p stock_picker([25,3,8,6,5,4,7,22,8,6])
-# p stock_picker([25,3,8,6,5,4,7,22,8,6,1])
+#p stock_picker([17,3,6,9,15,8,6,1,10])
+#p stock_picker([3,8,6,5,4,7,22,8,6])
+stock_picker([25,3,8,6,5,4,7,22,8,6])
+#p stock_picker([25,3,8,6,5,4,7,22,8,6,1])
+#p stock_picker([17,3,6,9,15,8,6,1,10])
